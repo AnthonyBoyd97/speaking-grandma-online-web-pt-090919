8c8ae82b8051f114430e1db9cd71e1a4ad5d055d
Anthony N Boyd <anboyd97@gmail.com>
2019-09-17 03:41:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(say)
   if say==say.upcase && say!="I LOVE YOU GRANDMA!"
-    puts "NO, NOT SINCE 1938!"
+    p "NO, NOT SINCE 1938!"
   elsif say==say.upcase && say=="I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
